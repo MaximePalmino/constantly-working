0156ae55237596919edace038528a9788c75555b
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-09-19 14:54:39 +0200

    [UPDATE]Mousemove

--- app.ts
+++ app.ts
@@ -1,5 +1,18 @@
-const { writeFileSync } = require('fs')
+const robot = require("robotjs");
+
+robot.setMouseDelay(2);
+
+const twoPI = Math.PI * 2.0;
+const screenSize = robot.getScreenSize();
+const height = (screenSize.height / 2) - 10;
+const width = screenSize.width;
+let y;
 
 setInterval(() => {
-    writeFileSync('./content/life-saver.txt', Math.random().toString())
+    for (var x = 0; x < width; x++)
+    {
+        y = height * Math.sin((twoPI * x) / width) + height;
+        robot.moveMouse(x, y);
+    }
 }, 10000)
+
